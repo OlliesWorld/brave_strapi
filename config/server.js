54f3fb5afac5f07ee25eb999dc0dafb8cd243fef
Roni <rtlockwoodwork@gmail.com>
2021-12-11 14:32:38 -0700

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'cab6081a8aaab695a81c10a4bf46b22b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5bf25203-a7bb-4802-ad80-13b7dd094733'
+};
