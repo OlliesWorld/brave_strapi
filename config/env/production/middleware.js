271c6f66bc1e234d8a24ecaafc2b4a52e26e0b28
Roni <rtlockwoodwork@gmail.com>
2021-12-24 10:35:22 -0700

    render config

---
+++ config/env/production/middleware.js
@@ -0,0 +1,12 @@
+
+   
+module.exports = {
+    settings: {
+      logger: {
+        level: "error",
+      },
+      public: {
+        path: "/data/public",
+      },
+    },
+  };
--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,5 +1,9 @@
 module.exports = ({ env }) => ({
-    url: env('https://brave-cards.herokuapp.com/'),
+    host: "0.0.0.0",
+    url: env("RENDER_EXTERNAL_URL"),
+    admin: {
+      auth: {
+        secret: env("ADMIN_JWT_SECRET"),
+      },
+    },
   });
- 
-  
